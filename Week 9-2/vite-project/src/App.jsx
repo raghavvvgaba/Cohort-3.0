803b5f8646230b043f84ab1f2b5d7c98addfb8a3
Raghav Gaba <158083556+raghavvvgaba@users.noreply.github.com>
2025-01-24 05:12:13 +0530

    Started React

--- Week 9-2/vite-project/src/App.jsx
+++ Week 9-2/vite-project/src/App.jsx
@@ -2,15 +2,21 @@ import { useState, useEffect } from "react"
 
 function App() {
   const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
 
   function increase(){
-    
+    setCount(c => c + 1);
+  }
+
+  function decrease(){
+    setCount2(c => c + 1)
   }
 
   return (
       <div>
-        <Counter count={count}/>
-        After counter
+        <Counter count={count} count2={count2}/>
+        <button onClick={increase}>Increase Count</button>
+        <button onClick={decrease}>Decrease Count</button>
       </div>
   )
 }
@@ -23,9 +29,19 @@ function Counter(props){
     return function(){
       console.log("unmount");
     }
-  }, []);
+  }, []); //this will be called on mounting 
+
+  useEffect(function(){
+    console.log("count has changed");
+
+    return function(){
+      console.log("cleanup inside second effect");
+    }     //when the value of count changes, it cleans up for the previous state and then run the effect for the next value of count
+  }, [props.count]) //this will be called when count changes
+
   return <div>
-    Counter {props.count}
+    Counter1 {props.count} <br />
+    Counter2 {props.count2} <br />
   </div>
 }
 
